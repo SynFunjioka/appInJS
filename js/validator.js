eaccfee60c012502f1a088a8e7d17a964d400a57
SynFunjioka <alanff2800@gmail.com>
2023-01-28 09:15:49 -0600

    validator class

--- js/validator.js
+++ js/validator.js
@@ -4,38 +4,80 @@
 class Form{
     constructor(formDOM){
         this.form = formDOM;
-        this.formGroups = $(this.form).children('.form-group');
+
+        this.formGroups = $(this.form).children('.form-group').toArray().map(group => {
+            return new InputGroup(group, Validators.email)
+        });
+        //* Defaults 
+
         this.submitBtn = $(formDOM).children('button').click(() => this.submit());
-        
         this.firstCheck = false;
         this.formValid = false;
     }
 
     submit(){
-        $(this.formGroups).children('input').each((index, dom) => {
-            console.log('funciona la clase', dom);
+        // console.log('INPUTS', this.formGroups);
+        this.formGroups.forEach(input => {
+            input.validate()
+        });
+
+        /* $(this.formGroups).children('input').each((index, dom) => {
+            // console.log('funciona la clase', dom);
             const input = $(dom).attr('name');
     
-            // TODO: Agrgar un onChangeEvent para detectar cuando el input este correcto y validado
-            switch(input){
-                case 'email':
-                    validate(dom, emailValidator);
-                    $(dom).change(() => validate(dom, emailValidator));
-            }
-        });
+            // switch(input){
+            //     case 'email':
+            //         validate(dom, emailValidator);
+            //         $(dom).change(() => validate(dom, emailValidator));
+            // }
+        }); */
     }
 }
 
-class input{
-    constructor(inputDOM){
-        this.input = inputDOM;
+class InputGroup{
+    constructor(inputGroupDOM, validator = undefined){
+        this.inputGroup = inputGroupDOM;
+        this.input = $(inputGroupDOM).children('input');
+        console.log(this.input);
+        this.validator = validator; //If this is undefined means that this field isn't required
+
+        //* Defaults 
+        this.isValid = this.validator ? false : true;
     }
-    isValid = false;
+
+    validate(){
+        if(!this.validator){ return}
+
+        const value = $(this.input).val();
+    
+        if(!this.validator(value)){
+            this.isValid = false;
+            this.inputError($(this.inputGroup));
+        }else{
+            this.isValid = true;
+            this.inputSuccess($(this.inputGroup));
+        }
+    }
+
+    onChange(){
+        this.validate();
+    }
+
+    //* MODIFIERS
+    inputError(formGroup){
+        $(formGroup).removeClass('form-group_success');
+        $(formGroup).addClass('form-group_error');
+    }
+    
+    inputSuccess(formGroup){
+        $(formGroup).removeClass('form-group_error');
+        $(formGroup).addClass('form-group_success');
+    }
 }
 
 class Validators{
-    static emailValidator(email){
-        return Regexs.email(email);
+    static email(email){
+        return Regexs.email.test(email);
     }
 }
 
